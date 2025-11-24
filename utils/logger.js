3df810fc07446a69a7245747076e12fe47a1e8c7
Joel Cerbito <joel.cerbito03@gmail.com>
2025-11-24 15:52:35 +0800

    updated logger.js - removed '[]' for all

--- utils/logger.js
+++ utils/logger.js
@@ -6,11 +6,11 @@ const customFormat = format.combine(
     format.printf(info => {
         let message = info.message;
         // If message contains [PASS], color it green
-        if (message.includes('[PASS]')) {
+        if (message.includes('PASS')) {
             message = `\x1b[32m${message}\x1b[0m`; // Green | For Pass
-        } else if (message.includes('[FAIL]') || message.includes('[ERROR]')) {
+        } else if (message.includes('FAIL') || message.includes('ERROR')) {
             message = `\x1b[31m${message}\x1b[0m`; // Red | For Error
-        } else if (message.includes('[CLICK]') || message.includes('[INPUT]') || message.includes('[KEY]')) {
+        } else if (message.includes('CLICK') || message.includes('INPUT') || message.includes('KEY')) {
             message = `\x1b[33m${message}\x1b[0m`; // Yellow | For Actions
         } else if (message.includes('SLEEP') || message.includes('INFO')) {
             message = `\x1b[34m${message}\x1b[0m`; // Blue | For info
