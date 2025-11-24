86b1bdf916b3c8c602ef3f65702cb8c28341524a
Joel Cerbito <joel.cerbito03@gmail.com>
2025-11-24 16:00:02 +0800

    updated test cases - to be continue

--- tests/loginHome.spec.js
+++ tests/loginHome.spec.js
@@ -6,17 +6,28 @@ const testData = JSON.parse(fs.readFileSync('./data/data.json'));
 
 test.beforeEach(async ({ common, login }) => {
   await common.openURL(url.getBaseURL());
-  await login.loginToApp(testData.standardAccount);
+  // await login.loginToApp(testData.standardAccount);
 });
 
 test.describe('Basic Login Flow', () => {
 
+  test.beforeEach('[Pre-requisite] login to app.', async ({ login }) => {
+    await login.loginToApp(testData.standardAccount);
+  });
+
   test('[TC 01] verify successful log in', async ({ login }) => {
-    await login.verifyLogin();
+    await login.verifyLogin("Success");
   });
 
   test('[TC 02] logout account', async ({ login }) => {
     await login.logoutToApp();
   });
 
+});
+
+test.describe('Negative Login Flow', () => {
+
+  test('[TC 01] login using invalid credentials', async ({ login }) => {
+    // TO DO:
+  })
 });
