77611d577816c20ab5cfb826182b802af1bcc681
Joel Cerbito <joel.cerbito03@gmail.com>
2026-03-18 08:56:02 +0800

    Change default to 'headless'

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   timeout: 60000,
   fullyParallel: true,
   use: {
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure'
     // video: 'retain-on-failure'
   },
